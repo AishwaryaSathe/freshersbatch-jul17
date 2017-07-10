15d7b31fdf0ef05658e35cf879859a6e082d0893
AishwaryaSathe <sathe.aishu@gmail.com>
2017-07-10 22:21:27 +0530

    Add files via upload

---
+++ assignments/JS/14_count.js
@@ -0,0 +1,9 @@
+function calc()
+{
+//var str=document.getElementById("t1").value;
+var str="how are you doing?";
+ document.write("<br>"+str);
+var n=str.length;
+document.write("<br>No. of character in the sentence= "+n);
+
+}
